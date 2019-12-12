e129654fb950465130777561fe9f2155d3260065
dinhmai74 <dinh.mai@nextfunc.com>
2019-12-12 18:41:06 +0700

    add text style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { PrimaryRouters } from "router/PrimaryRouters";
 
