e07726a86f56ce716deef282c03ed4972fe92d46
dinhmai74 <dinh.mai@nextfunc.com>
2019-12-16 06:30:09 +0700

    edit style text

--- stories/2-Text.stories.js
+++ stories/2-Text.stories.js
@@ -11,8 +11,8 @@ export default {
 export const Text = () => (
   <div>
     <p className="text__b1">normal one</p>
-    <p className="text__h1--grey">b1 primary</p>
-    <p className="text__b1--blue-grey">b1 blue-grey</p>
+    <p className="text__h1 color__primary">b1 primary</p>
+    <p className="text__b1 color__blue-grey">b1 blue-grey</p>
   </div>
 );
 
