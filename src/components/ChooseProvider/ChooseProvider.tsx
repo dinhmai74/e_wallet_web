c64d9bc121bfb7c07a9ed0eb7d1fed8d82fbc28e
CuBilll <16521053@gm.uit.edu.vn>
2020-01-02 16:00:09 +0700

    add event onclick go buy phone card detail

--- src/components/ChooseProvider/ChooseProvider.tsx
+++ src/components/ChooseProvider/ChooseProvider.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { images } from "theme";
 import { ItemProvider } from "components/ItemProvider/ItemProvider";
+import { useHistory } from "react-router";
+import { Paths } from "router/PrimaryRouters";
 
 export const ChooseProvider: React.FC = () => {
+  const history = useHistory();
   return (
     <div
       className=" d-flex flex-wrap  px-10 py-10 mb-64  justify-center "
@@ -10,9 +13,24 @@ export const ChooseProvider: React.FC = () => {
     >
       <p className="`text__h2 color__steel font-bold pl-16">Choose Provider:</p>
       <div className="flex flex-row  py-10 px-12 flex-wrap ">
-       <ItemProvider src={images.iconProvider.viettel}/>
-       <ItemProvider src={images.iconProvider.mobiphone}/>
-       <ItemProvider src={images.iconProvider.vtc}/>
+        <ItemProvider
+          src={images.iconProvider.viettel}
+          onClick={() => {
+            history.push(Paths.buyPhoneCardDetail);
+          }}
+        />
+        <ItemProvider
+          src={images.iconProvider.mobiphone}
+          onClick={() => {
+            history.push(Paths.buyPhoneCardDetail);
+          }}
+        />
+        <ItemProvider
+          src={images.iconProvider.vtc}
+          onClick={() => {
+            history.push(Paths.buyPhoneCardDetail);
+          }}
+        />
       </div>
     </div>
   );
