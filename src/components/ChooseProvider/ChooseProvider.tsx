a4f5d5c778b822bbe498f85d7ff247ad5937cb5a
Huynhgiaphat12 <16520910@gm.uit.edu.vn>
2020-01-04 01:07:31 +0700

    remove unused

--- src/components/ChooseProvider/ChooseProvider.tsx
+++ src/components/ChooseProvider/ChooseProvider.tsx
@@ -1,10 +1,8 @@
-import { Card, CardMedia } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import React from "react";
-import { images } from "theme";
 import { ItemProvider } from "components/ItemProvider/ItemProvider";
+import React from "react";
 import { useHistory } from "react-router";
 import { Paths } from "router/PrimaryRouters";
+import { images } from "theme";
 
 export const ChooseProvider: React.FC = () => {
   const history = useHistory();
