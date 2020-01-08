a90ca04f69e11ee694280d02c55277666fc8d4b9
CuBilll <16521053@gm.uit.edu.vn>
2020-01-08 11:59:06 +0700

    refactor code buy phone card payment and success screen

--- src/screens/buy-phone-card/BuyPhoneCardPayMent/BuyPhoneCardPayMent.tsx
+++ src/screens/buy-phone-card/BuyPhoneCardPayMent/BuyPhoneCardPayMent.tsx
@@ -1,30 +1,14 @@
-import { Screen } from "components";
-import { PaymentMethods } from "components/PaymentMethods";
 import React from "react";
-import { useBoolean } from "react-use";
-import { InfoPaymentCard } from "screens/buy-phone-card/BuyPhoneCardPayMent/component/InfoPaymentCard";
-import { images } from "theme";
+import { PaymentScreen } from "components/PaymentScreen";
 
 export const BuyPhoneCardPayMent: React.FC = props => {
-  const [, setSelectedPayment] = useBoolean(false);
-
-  const onChangeMethods = () => {
-    setSelectedPayment(true);
-  };
-
   return (
-    <Screen>
-      <PaymentMethods onChange={onChangeMethods} />
-      <InfoPaymentCard
-        type="left"
-        src={images.phoneCardPayment.payment}
-        navigateTo="/payment/buy-phone-card/success"
-        buttonTx="Confirm"
-        provider="viettel"
-        price={"100.000"}
-        quantity="3"
-        totalAmount="300.000d"
-      />
-    </Screen>
+    <PaymentScreen
+      titleProvider="viettel"
+      price="100.000"
+      quantity="3"
+      totalAmount="300.000d"
+      navigateTo="/payment/buy-phone-card/success"
+    />
   );
 };
