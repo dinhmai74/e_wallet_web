d066a41a395765b1a2e019acfe6da0d574b79693
dinhmai74 <anh.dinh74@gmail.com>
2020-01-10 12:33:41 +0700

    Updates

---
+++ precache-manifest.adb7ad37841c258fbdc86834364cd310.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0749cd758a00d690342b8b5e1ac2837",
+    "url": "/e_wallet_web/index.html"
+  },
+  {
+    "revision": "e2926389019bf9e3c979",
+    "url": "/e_wallet_web/static/css/2.9adeaed4.chunk.css"
+  },
+  {
+    "revision": "3e3445a7c1e07566b2c6",
+    "url": "/e_wallet_web/static/css/main.45b9ae8f.chunk.css"
+  },
+  {
+    "revision": "e2926389019bf9e3c979",
+    "url": "/e_wallet_web/static/js/2.3d035c36.chunk.js"
+  },
+  {
+    "revision": "cda97790368e21d8bd266221efa4d457",
+    "url": "/e_wallet_web/static/js/2.3d035c36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e3445a7c1e07566b2c6",
+    "url": "/e_wallet_web/static/js/main.3ce11922.chunk.js"
+  },
+  {
+    "revision": "ce6fd547cc87b032d5c1",
+    "url": "/e_wallet_web/static/js/runtime-main.9536a1ae.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/e_wallet_web/static/media/404.6695af99.svg"
+  },
+  {
+    "revision": "687435829d93482a3521e780993d5eca",
+    "url": "/e_wallet_web/static/media/add.68743582.svg"
+  },
+  {
+    "revision": "1d847751b0f1be161e34558895c34142",
+    "url": "/e_wallet_web/static/media/arrow-half-right.1d847751.svg"
+  },
+  {
+    "revision": "8aaeabdf1ecffccdefc9753cb1c53c06",
+    "url": "/e_wallet_web/static/media/ic_starfull.8aaeabdf.svg"
+  },
+  {
+    "revision": "856ddf6db469f1d8ef69324c58daf310",
+    "url": "/e_wallet_web/static/media/icon_star_empty.856ddf6d.svg"
+  },
+  {
+    "revision": "88327b0c4ee4f74d0adf045c4f2ffc2c",
+    "url": "/e_wallet_web/static/media/icon_star_half.88327b0c.svg"
+  },
+  {
+    "revision": "ba19cb3a6327f061a5ed59d4ba108704",
+    "url": "/e_wallet_web/static/media/menu.ba19cb3a.svg"
+  },
+  {
+    "revision": "02104868c3c823727e9f0dac5e87d538",
+    "url": "/e_wallet_web/static/media/minus.02104868.svg"
+  },
+  {
+    "revision": "c1cfd30d6a93b247256efd0243f4204a",
+    "url": "/e_wallet_web/static/media/movies.c1cfd30d.svg"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/e_wallet_web/static/media/notFound.8c3d2a79.svg"
+  },
+  {
+    "revision": "43d9f3dea58898fac573da68e6398c3b",
+    "url": "/e_wallet_web/static/media/payment.43d9f3de.svg"
+  },
+  {
+    "revision": "a5798b200dbd924df9dc615ad5b9c334",
+    "url": "/e_wallet_web/static/media/screen.a5798b20.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.153994c03f9f10db42d82913bc041184.js"
+  "/e_wallet_web/precache-manifest.adb7ad37841c258fbdc86834364cd310.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e_wallet_web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
