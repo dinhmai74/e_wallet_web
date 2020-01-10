f87bcbe9476dd931d8aa6b92e42b97ceb796d544
dinhmai74 <anh.dinh74@gmail.com>
2020-01-10 11:36:57 +0700

    Updates

---
+++ precache-manifest.051edea01edb9b86bdd3a0a370098035.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09ee3268f426118c8954082e39ff7cc",
+    "url": "/e_wallet_web/index.html"
+  },
+  {
+    "revision": "58765f100f652f10b5a6",
+    "url": "/e_wallet_web/static/css/2.9adeaed4.chunk.css"
+  },
+  {
+    "revision": "55d20d036655ba626e41",
+    "url": "/e_wallet_web/static/css/main.45b9ae8f.chunk.css"
+  },
+  {
+    "revision": "58765f100f652f10b5a6",
+    "url": "/e_wallet_web/static/js/2.615c3282.chunk.js"
+  },
+  {
+    "revision": "cda97790368e21d8bd266221efa4d457",
+    "url": "/e_wallet_web/static/js/2.615c3282.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55d20d036655ba626e41",
+    "url": "/e_wallet_web/static/js/main.442a2849.chunk.js"
+  },
+  {
+    "revision": "ce6fd547cc87b032d5c1",
+    "url": "/e_wallet_web/static/js/runtime-main.9536a1ae.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/e_wallet_web/static/media/404.6695af99.svg"
+  },
+  {
+    "revision": "687435829d93482a3521e780993d5eca",
+    "url": "/e_wallet_web/static/media/add.68743582.svg"
+  },
+  {
+    "revision": "1d847751b0f1be161e34558895c34142",
+    "url": "/e_wallet_web/static/media/arrow-half-right.1d847751.svg"
+  },
+  {
+    "revision": "8aaeabdf1ecffccdefc9753cb1c53c06",
+    "url": "/e_wallet_web/static/media/ic_starfull.8aaeabdf.svg"
+  },
+  {
+    "revision": "856ddf6db469f1d8ef69324c58daf310",
+    "url": "/e_wallet_web/static/media/icon_star_empty.856ddf6d.svg"
+  },
+  {
+    "revision": "88327b0c4ee4f74d0adf045c4f2ffc2c",
+    "url": "/e_wallet_web/static/media/icon_star_half.88327b0c.svg"
+  },
+  {
+    "revision": "ba19cb3a6327f061a5ed59d4ba108704",
+    "url": "/e_wallet_web/static/media/menu.ba19cb3a.svg"
+  },
+  {
+    "revision": "02104868c3c823727e9f0dac5e87d538",
+    "url": "/e_wallet_web/static/media/minus.02104868.svg"
+  },
+  {
+    "revision": "659c50646f0e6b0b1492342acb307bec",
+    "url": "/e_wallet_web/static/media/movies.659c5064.svg"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/e_wallet_web/static/media/notFound.8c3d2a79.svg"
+  },
+  {
+    "revision": "43d9f3dea58898fac573da68e6398c3b",
+    "url": "/e_wallet_web/static/media/payment.43d9f3de.svg"
+  },
+  {
+    "revision": "a5798b200dbd924df9dc615ad5b9c334",
+    "url": "/e_wallet_web/static/media/screen.a5798b20.svg"
+  }
+]);
